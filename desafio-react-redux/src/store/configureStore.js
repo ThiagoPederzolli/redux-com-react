a96c3622c27710d1a647ba9ec7e77fd1ceeefa1d
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-11-09 10:20:47 -0300

    Component e store de Photos

--- desafio-react-redux/src/store/configureStore.js
+++ desafio-react-redux/src/store/configureStore.js
@@ -3,12 +3,12 @@ import {
   configureStore,
   getDefaultMiddleware,
 } from '@reduxjs/toolkit';
-
+import photos from './photos';
 import login from './login';
 import localStorage from './middleware/localStorage';
 
 const middleware = [...getDefaultMiddleware(), localStorage];
-const reducer = combineReducers({ login });
+const reducer = combineReducers({ login, photos });
 
 const store = configureStore({ reducer, middleware });
 export default store;
